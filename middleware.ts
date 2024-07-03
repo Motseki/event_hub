0c2c651003ac328c9a2a548ff29e282d62d460c6
Motseki <lbmotseki@gmail.com>
2024-07-03 17:10:07 +0200

    Middleware Correction

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,50 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+// import { authMiddleware } from "@clerk/nextjs/server";
 
  
+// export default authMiddleware({
+//   publicRoutes: [
+//     '/',
+//     '/events/:id',
+//     '/api/webhooks/clerk(.*)',
+//     '/api/webhooks/clerk',
+//     '/api/webhooks/stripe',
+//     '/api/uploadthing'
+//   ],
+//   ignoredRoutes: [
+//     '/api/webhooks/clerk',
+//     '/api/webhooks/stripe',
+//     '/api/uploadthing'
+//   ]
+// });
+ 
+// export const config = {
+//   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
+ 
+
+import { authMiddleware } from "@clerk/nextjs/server";
+
 export default authMiddleware({
   publicRoutes: [
     '/',
     '/events/:id',
-    '/api/webhook/clerk',
-    '/api/webhook/stripe',
+    // '/api/webhooks(.*)', // Clerk webhook routes as public
+    `/api/webhooks/(.*)` ,
+    '/api/webhooks',
+    '/api/webhooks/stripe',
     '/api/uploadthing'
   ],
   ignoredRoutes: [
-    '/api/webhook/clerk',
-    '/api/webhook/stripe',
+    '/api/webhooks', // Clerk webhook routes to be ignored by auth
+    '/api/webhooks/stripe',
     '/api/uploadthing'
   ]
 });
- 
+
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    '/((?!.+\\.[\\w]+$|_next).*)', 
+    '/', 
+    '/(api|trpc)(.*)'
+  ],
 };
- 
